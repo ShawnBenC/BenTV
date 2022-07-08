47dc42e3f7d6f9d1346e60daf5dadbc2d794c8c6
gaigai12 <107260398+gaigai12@users.noreply.github.com>
2022-07-08 20:57:51 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitea.com/gaigai/gougou/raw/branch/main/gai0619.txt");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
